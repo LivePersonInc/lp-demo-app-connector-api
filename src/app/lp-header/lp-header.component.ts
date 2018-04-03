12b66bde8dbe08aca91bb5c330f6e035cbfb3114
eplaza <eplaza@liveperson.com>
2018-04-03 15:33:10 +0200

    header component created

---
+++ src/app/lp-header/lp-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lp-header',
+  templateUrl: './lp-header.component.html',
+  styleUrls: ['./lp-header.component.css']
+})
+export class LpHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
