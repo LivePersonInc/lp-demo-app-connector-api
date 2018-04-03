622c325168afa66d7b9ca7ada5e6f5dc627ab55b
eplaza <eplaza@liveperson.com>
2018-04-03 17:08:27 +0200

    conversation component created

---
+++ src/app/lp-conversation/lp-conversation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lp-conversation',
+  templateUrl: './lp-conversation.component.html',
+  styleUrls: ['./lp-conversation.component.css']
+})
+export class LpConversationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
