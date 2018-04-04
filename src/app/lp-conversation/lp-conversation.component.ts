67ad09f89fba1ff5bdcc2b612ceb07cdf820e1af
eplaza <eplaza@liveperson.com>
2018-04-04 11:49:29 +0200

    Conversation helper class

--- src/app/lp-conversation/lp-conversation.component.ts
+++ src/app/lp-conversation/lp-conversation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {environment} from "../../environments/environment";
+import {MatSnackBar} from "@angular/material";
+import {SendApiService} from "../services/send-api.service";
 
 @Component({
   selector: 'app-lp-conversation',
@@ -10,13 +12,18 @@ export class LpConversationComponent implements OnInit {
   public brandId;
   public appKey;
   public appSecret;
-  constructor() { }
+  public isConversationStared: Boolean;
+  constructor(public snackBar: MatSnackBar,public sendApiService: SendApiService) { }
 
   ngOnInit() {
 
     this.brandId = environment.brandId;
     this.appKey = environment.appKey;
     this.appSecret = environment.appSecret;
+  }
+
+  public startConversation() {
+
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   umsDomain: "localhost",
   sentinel: "lp-sentinel-web-qa.dev.lprnd.net",
   idp:"qtvr-wng113.dev.lprnd.net",
-  brandId: "le28440506",
-  appKey: "140b14e5-fa56-4e17-b18f-7b5fa3efddc0",
-  appSecret: "2a5srg0shkhm2t4u4dknqtlkhj"
+  brandId: "le92127075",
+  username:"epbot@liveperson.com",
+  appKey: "7791627d-ce48-4363-8943-ac9c8fa7ca3b",
+  appSecret: "c79p3jifp73ohupjkvirjkfj9e"
 };
