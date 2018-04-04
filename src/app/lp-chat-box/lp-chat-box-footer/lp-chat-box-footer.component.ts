482a8e3020c8de94fedd6c50c177f4911a54cf54
eplaza <eplaza@liveperson.com>
2018-04-04 09:38:45 +0200

    chat footer

---
+++ src/app/lp-chat-box/lp-chat-box-footer/lp-chat-box-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lp-chat-box-footer',
+  templateUrl: './lp-chat-box-footer.component.html',
+  styleUrls: ['./lp-chat-box-footer.component.css']
+})
+export class LpChatBoxFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
