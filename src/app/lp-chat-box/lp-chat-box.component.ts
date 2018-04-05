7eb6f88ae1d43091eedd8af19b381c0038a5846a
eplaza <eplaza@liveperson.com>
2018-04-05 16:57:50 +0200

    show user name

--- src/app/lp-chat-box/lp-chat-box.component.ts
+++ src/app/lp-chat-box/lp-chat-box.component.ts
@@ -8,16 +8,12 @@ import {ConversationManager} from "../util/ConversationManager";
   styleUrls: ['./lp-chat-box.component.css']
 })
 export class LpChatBoxComponent implements OnInit {
-  public testMessage: ChatMessage;
-  public testReceivedMessage: ChatMessage;
   @Input()  conversationManager: ConversationManager;
   @Output() onSendMessage = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
-    this.testReceivedMessage = new ChatMessage("received", "1522829611", "Test received message", "Manuel Test", "XX");
-    this.testMessage = new ChatMessage("sent", "1522829611", "Test sent message", "Manuel Test", "XX");
   }
 
   public sendMessage(message) {
