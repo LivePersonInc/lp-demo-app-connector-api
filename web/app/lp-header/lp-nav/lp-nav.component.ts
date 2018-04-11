9c1caa838bda37c398c421c893d1d05873848cc1
eplaza <eplaza@liveperson.com>
2018-04-11 11:32:18 +0200

    basic routing added

---
+++ web/app/lp-header/lp-nav/lp-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lp-nav',
+  templateUrl: './lp-nav.component.html',
+  styleUrls: ['./lp-nav.component.scss']
+})
+export class LpNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
