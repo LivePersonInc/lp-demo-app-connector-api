ef7f3317e9567163fb5d762586ddb65368ea8029
eplaza <eplaza@liveperson.com>
2018-04-11 13:20:38 +0200

    Animations when pages are loaded

--- web/app/lp-home/lp-home.component.ts
+++ web/app/lp-home/lp-home.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {fadeInAnimation} from "../shared/animations/lp-animations";
 
 @Component({
   selector: 'lp-home',
   templateUrl: './lp-home.component.html',
-  styleUrls: ['./lp-home.component.scss']
+  styleUrls: ['./lp-home.component.scss'],
+  animations: [fadeInAnimation],
+  host: {'[@fadeInAnimation]': ''}
 })
 export class LpHomeComponent implements OnInit {
   firstFormGroup: FormGroup;
