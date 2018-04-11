2c5928994ed3976433177f3ac3c2921e8dc97934
eplaza <eplaza@liveperson.com>
2018-04-11 13:53:15 +0200

    style fix

--- web/app/lp-start-tutotial/lp-start-tutotial.component.ts
+++ web/app/lp-start-tutotial/lp-start-tutotial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-lp-start-tutotial',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LpStartTutotialComponent implements OnInit {
 
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
 }
