c068ae42b4beeff7e67e4ad6bad53586cc69ad56
eplaza <eplaza@liveperson.com>
2018-04-11 13:35:19 +0200

    Start tutorial page created

---
+++ web/app/lp-start-tutotial/lp-start-tutotial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lp-start-tutotial',
+  templateUrl: './lp-start-tutotial.component.html',
+  styleUrls: ['./lp-start-tutotial.component.scss']
+})
+export class LpStartTutotialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
