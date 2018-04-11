aa032a6283f5bd639ab556695c9081d1984fcf5c
eplaza <eplaza@liveperson.com>
2018-04-11 12:16:07 +0200

    Responsive Nav

---
+++ web/app/lp-header/lp-nav-menu/lp-nav-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lp-nav-menu',
+  templateUrl: './lp-nav-menu.component.html',
+  styleUrls: ['./lp-nav-menu.component.scss']
+})
+export class LpNavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
