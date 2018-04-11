268eb8aec920fb3f170b18073586add7e0f3a454
eplaza <eplaza@liveperson.com>
2018-04-11 11:35:14 +0200

    Demo page created

---
+++ web/app/lp-demo/lp-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lp-demo',
+  templateUrl: './lp-demo.component.html',
+  styleUrls: ['./lp-demo.component.scss']
+})
+export class LpDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
