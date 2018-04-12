fd64700fdb6ea6d51629d2bb0bc599d660e65d99
eplaza <eplaza@liveperson.com>
2018-04-12 11:20:05 +0200

    New components created

---
+++ web/app/lp-start-tutotial/webhooks-config/webhooks-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lp-webhooks-config',
+  templateUrl: './webhooks-config.component.html',
+  styleUrls: ['./webhooks-config.component.scss']
+})
+export class WebhooksConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
