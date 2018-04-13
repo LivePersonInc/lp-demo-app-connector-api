d2cc0fc1296a6ee5ccec60f145cd127e22f19406
eplaza <eplaza@liveperson.com>
2018-04-13 12:53:52 +0200

    Authentication service

--- web/app/services/authentication.service.ts
+++ web/app/services/authentication.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 import {SendApiService} from "./send-api.service";
+import {ErrorObservable} from "rxjs/observable/ErrorObservable";
+import {catchError} from "rxjs/operators";
 
 @Injectable()
 export class AuthenticationService {
@@ -14,16 +16,16 @@ export class AuthenticationService {
   //Barer Token
   public login(brandId: string, username: string, password: string) {
     this.sendApiService.startLoading();
-    return this.http.post<any>(`https://ctvr-ano041.dev.lprnd.net/api/account/${brandId}/login`, { username: username, password: password })
-      .map(user => {
-        // login successful if there's a jwt token in the response
-        console.log(user);
-        if (user && user.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify(user));
-        }
+    console.log("LOGFIN");
+    this.http.post<any>(`https://ctvr-ano041.dev.lprnd.net/api/account/${brandId}/login`, { username: username, password: password }, {Accept: "application/json", "Content-Type": "application/json"})
+      .pipe(
+        catchError(this.handleError)
+      ).subscribe(res => {
+        console.log(res);
+         this.sendApiService.stopLoading();
+      },error => {
         this.sendApiService.stopLoading();
-        return user;
+
       });
   }
 
@@ -32,5 +34,18 @@ export class AuthenticationService {
     localStorage.removeItem('currentUser');
   }
 
-
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${JSON.stringify(error.status)}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an ErrorObservable with a user-facing error message
+    return new ErrorObservable('Something bad happened; please try again later.');
+  }
 }
