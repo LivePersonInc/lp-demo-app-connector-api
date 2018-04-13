275be2b1e0a982aa059008d90c1416a2ba3e0622
eplaza <eplaza@liveperson.com>
2018-04-13 15:45:16 +0200

    auth guards

--- web/app/core/guards/auth-guard.guard.ts
+++ web/app/core/guards/auth-guard.guard.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import {AuthenticationService} from "../services/authentication.service";
 
 @Injectable()
 export class AuthGuardGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {}
+  canActivate()  {
+    if(this.authenticationService.getUser()) {
+      return true;
+    }
+    this.router.navigateByUrl('/home')
+    return false;
   }
 }
