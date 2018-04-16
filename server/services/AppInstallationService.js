79e45dd65d0484bc11b2fef23396e5e51dfcd08f
eplaza <eplaza@liveperson.com>
2018-04-16 14:16:51 +0200

    Account Config and appInstalation brige

--- server/services/AppInstallationService.js
+++ server/services/AppInstallationService.js
@@ -10,11 +10,11 @@ class AppInstallationService {
         this.client = new Client();
     }
 
-    installNewApp(args) {
+    installNewApp(brandId,args) {
         return new Promise((resolve, reject) => {
             return this.client
                 .post(
-                    `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${this.nconf.get("BRAND_ID")}/configuration/app-install/installations?v=1.0`,
+                    `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${brandId}/configuration/app-install/installations?v=1.0`,
                     args,
                     function (data, response) {
                         resolve([data, response]);
@@ -26,11 +26,11 @@ class AppInstallationService {
         });
     }
 
-    uninstallAppById(app_id, args) {
+    uninstallAppById(app_id, brandId, args) {
         return new Promise((resolve, reject) => {
             return this.client
                 .delete(
-                    `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${this.nconf.get("BRAND_ID")}/configuration/app-install/installations/${app_id}?v=1.0`,
+                    `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${brandId}/configuration/app-install/installations/${app_id}?v=1.0`,
                     args,
                     function (data, response) {
                         resolve([data, response]);
@@ -42,11 +42,27 @@ class AppInstallationService {
         });
     }
 
-    getAppById(app_id, args) {
+  getAppsForBrandId(brandId, args) {
+    return new Promise((resolve, reject) => {
+      return this.client
+        .get(
+          `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${brandId}/configuration/app-install/installations?v=1.0`,
+          args,
+          function (data, response) {
+            resolve([data, response]);
+          }
+        )
+        .on("error", e => {
+          reject(e);
+        });
+    });
+  }
+
+    getAppById(app_id,brandId, args) {
         return new Promise((resolve, reject) => {
             return this.client
                 .get(
-                    `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${this.nconf.get("BRAND_ID")}/configuration/app-install/installations/${app_id}?v=1.0`,
+                    `https://${this.nconf.get("APP_INSTALLATION_SERVER") || domains.getDomainByServiceName('accountConfigReadWrite')}/api/account/${brandId}/configuration/app-install/installations/${app_id}?v=1.0`,
                     args,
                     function (data, response) {
                         resolve([data, response]);
