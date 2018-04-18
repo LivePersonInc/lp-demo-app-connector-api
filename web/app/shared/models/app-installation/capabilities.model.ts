d20ca25c90cbdc3f8ae5d8911e459e87b5bcf572
eplaza <eplaza@liveperson.com>
2018-04-18 12:43:18 +0200

    AppInstallation deserialize bug fixed

--- web/app/shared/models/app-installation/capabilities.model.ts
+++ web/app/shared/models/app-installation/capabilities.model.ts
@@ -1,11 +1,13 @@
 import {Deserializable} from "../deserializable.model";
+import {Webhooks} from "./webhooks.model";
 
 export class Capabilities implements Deserializable<Capabilities>{
   engagement: object;
-  webhooks: object;
+  webhooks: Webhooks;
 
   deserialize(input: any): Capabilities {
     Object.assign(this, input);
+    input.webhooks ? this.webhooks = new Webhooks().deserialize(input.webhooks): this.webhooks = null;
     return this;
   }
 
--- web/app/shared/models/app-installation/endpoint.model.ts
+++ web/app/shared/models/app-installation/endpoint.model.ts
@@ -1,5 +1,13 @@
-export class Endpoint{
+import {Deserializable} from "../deserializable.model";
+
+export class Endpoint implements Deserializable<Endpoint>
+{
   endpoint: string;
   headers: Array<any>;
   max_retries: number;
+
+  deserialize(input: any): Endpoint {
+    Object.assign(this, input);
+    return this;
+  }
 }
