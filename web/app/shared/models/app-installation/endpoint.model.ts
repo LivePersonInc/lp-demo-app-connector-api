99d2574ffb6e7ec2acf4003ca8ea810d1e122d35
eplaza <eplaza@liveperson.com>
2018-04-18 15:26:13 +0200

    Webhooks update

--- web/app/shared/models/app-installation/endpoint.model.ts
+++ web/app/shared/models/app-installation/endpoint.model.ts
@@ -5,7 +5,9 @@ export class Endpoint implements Deserializable<Endpoint>
   endpoint: string;
   headers: Array<any>;
   max_retries: number;
-
+  constructor(){
+    this.endpoint = "https://";
+  }
   deserialize(input: any): Endpoint {
     Object.assign(this, input);
     return this;
