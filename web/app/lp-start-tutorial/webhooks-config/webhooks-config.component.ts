f30666bb3bd4328f6bee9c3ee2a6e451e6744509
eplaza <eplaza@liveperson.com>
2018-04-19 15:22:33 +0200

    Logic improved

--- web/app/lp-start-tutorial/webhooks-config/webhooks-config.component.ts
+++ web/app/lp-start-tutorial/webhooks-config/webhooks-config.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnChanges, OnInit} from '@angular/core';
 import {InstallationService} from "../../core/services/istallation.service";
 import {hasOwnProperty} from "tslint/lib/utils";
+import {Webhooks} from "../../shared/models/app-installation/webhooks.model";
 
 @Component({
   selector: 'lp-webhooks-config',
@@ -9,12 +10,22 @@ import {hasOwnProperty} from "tslint/lib/utils";
 })
 export class WebhooksConfigComponent implements OnInit {
 
+  public webhooks: Webhooks;
+
   constructor(private installationService:InstallationService) { }
 
   ngOnInit() {
+    this.installationService.istallationSubject.subscribe( event => {
+      if(event === 'APP_SELECTED') {
+        this.webhooks = this.installationService.selectedApp.capabilities.webhooks;
+        console.log("SFSFSG");
+        console.log(this.webhooks);
+      }
+    });
   }
 
   public updateWebhooks() {
+    /*
     console.log("UPDATE");
     for(let prop in this.installationService.selectedApp.capabilities.webhooks){
       if(this.installationService.selectedApp.capabilities.webhooks[prop].endpoint.length === 0){
@@ -23,6 +34,7 @@ export class WebhooksConfigComponent implements OnInit {
     }
     console.log(this.installationService.selectedApp);
     this.installationService.updateApp(this.installationService.selectedApp);
+    */
   }
 
 }
