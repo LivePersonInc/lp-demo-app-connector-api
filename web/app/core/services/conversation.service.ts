b92da4f03606075bc15b3eb54a8e57393f65dddd
eplaza <eplaza@liveperson.com>
2018-04-20 13:45:25 +0200

    State fixed

--- web/app/core/services/conversation.service.ts
+++ web/app/core/services/conversation.service.ts
@@ -14,41 +14,39 @@ import {Conversation} from "../../shared/models/conversation/conversation";
 @Injectable()
 export class ConversationService extends HttpService{
 
-  public  conversationEventSubject = new Subject<ConversationEvent>();
-  private conversationList; // Hash table of Conversation accesed by convID
+  public conversationEventSubject = new Subject<ConversationEvent>();
+  public conversation: Conversation;
+  //public conversationList; // Hash table of Conversation accesed by convID
 
   constructor(protected snackBar: MatSnackBar,protected http: HttpClient, protected loadingService:LoadingService, protected  sendApiService: SendApiService){
     super(snackBar,http, loadingService);
-    this.conversationList = [];
 
   }
 
-  public openConversation(userName: string, brandId: string, appKey: string, appSecret,initialMessage: string): Conversation {
+  public openConversation(userName: string, brandId: string, appKey: string, appSecret,initialMessage: string) {
 
-    let conversation: Conversation = new Conversation(this.snackBar, this.sendApiService, brandId, appKey, appSecret, userName, this.loadingService);
-    conversation.getAppJWT().then(resolve => {
-      conversation.getAppConsumerJWS().then(resolve => {
-        conversation.openConversation(initialMessage).then(conversationId => {
-          conversation.isConvStarted = true;
-          conversation.subscribeToMessageNotifications(conversationId);
-          this.conversationList[conversationId] = conversation;
+    this.conversation = new Conversation(this.snackBar, this.sendApiService, brandId, appKey, appSecret, userName, this.loadingService);
+    this.conversation.getAppJWT().then(resolve => {
+      this.conversation.getAppConsumerJWS().then(resolve => {
+        this.conversation.openConversation(initialMessage).then(conversationId => {
+          this.conversation.isConvStarted = true;
+          this.conversation.subscribeToMessageNotifications(conversationId);
           this.conversationEventSubject.next(new ConversationEvent(conversationId, ConvEvent.OPEN));
         });
       });
     });
-    //returns reference
-    return conversation;
+
   }
 
   public closeConversation(conversationId: string) {
-    this.conversationList[conversationId].closeConversation();
-    this.conversationList[conversationId].unsubscribeToMessageNotifications();
-    this.conversationList[conversationId].isConvStarted = false;
+    this.conversation.closeConversation();
+    this.conversation.unsubscribeToMessageNotifications();
+    this.conversation.isConvStarted = false;
   }
 
   public sendMessage(message:string, conversationId: string) {
-    if(this.conversationList[conversationId].isConvStarted) {
-      this.conversationList[conversationId].sendMessage(message).then(function () {
+    if(this.conversation.isConvStarted) {
+      this.conversation.sendMessage(message).then(function () {
         this.conversationEventSubject.next(new ConversationEvent(conversationId, ConvEvent.MESSAGE_SENT));
       }.bind(this));
     }else{
