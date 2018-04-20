94134c9569071dab7f0ea4b9b7c3aabce3414655
eplaza <eplaza@liveperson.com>
2018-04-20 13:45:25 +0200

    State fixed

--- web/app/lp-conversation/lp-conversation.component.ts
+++ web/app/lp-conversation/lp-conversation.component.ts
@@ -7,7 +7,7 @@ import {EventSourcePolyfill} from 'ng-event-source';
 import {LpChatBoxComponent} from "../lp-chat-box/lp-chat-box.component";
 import {LoadingService} from "../core/services/loading.service";
 import {ConversationService} from "../core/services/conversation.service";
-import {ConversationEvent} from "../shared/models/conversation/conversationEvent.model";
+import {ConversationEvent, ConvEvent} from "../shared/models/conversation/conversationEvent.model";
 
 @Component({
   selector: 'lp-conversation',
@@ -17,32 +17,38 @@ import {ConversationEvent} from "../shared/models/conversation/conversationEvent
 export class LpConversationComponent implements OnInit {
   public brandId: string;
   public conversation: Conversation;
-  public isConvStarted: boolean;
   public appKey: string;
   public appSecret: string;
-  public eventSource: EventSourcePolyfill;
   public userName: string;
-
-  @ViewChild('chatbox') private chatBox: LpChatBoxComponent;
 
   constructor(public snackBar: MatSnackBar,public sendApiService: SendApiService, private zone: NgZone, private  loadingService: LoadingService, private  conversationService: ConversationService) { }
 
   ngOnInit() {
+    console.log("ON INIT");
     this.brandId = environment.brandId;
     this.appKey = environment.appKey;
     this.appSecret = environment.appSecret;
     this.userName = "test user name";
+    if(this.conversationService.conversation){
+      this.conversation = this.conversationService.conversation;
+    }
 
     this.conversationService.conversationEventSubject.subscribe( (event:ConversationEvent) => {
-       if(event.conversationId === this.conversation.conversationId){
+       if(this.conversationService.conversation && event.conversationId === this.conversationService.conversation.conversationId){
          console.log(event.event);
+         if(event.event === ConvEvent.OPEN ){
+           this.conversation = this.conversationService.conversation;
+         }
+
        }
-    })
+    });
+
+
   }
 
   public startConversation(initialMessage: string) {
 
-    this.conversation = this.conversationService.openConversation(this.userName, this.brandId, this.appKey, this.appSecret, initialMessage);
+    this.conversationService.openConversation(this.userName, this.brandId, this.appKey, this.appSecret, initialMessage);
 
   }
 
