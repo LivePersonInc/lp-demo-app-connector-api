f810d4cfc47bc0602a328afa353d71c752e1ac3f
eplaza <eplaza@liveperson.com>
2018-04-27 13:28:40 +0200

    Login service is done in server brige

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ const installationBrige = require('./server/appInstallationBrige');
 const accountConfBrige = require('./server/accountConfBrige');
 const notifications = require('./server/notifications');
 const domains = require('./server/connector/CsdsProperties');
+const authenticationBrige = require('./server/authenticationBrige');
 const https = require('https');
 const forceSsl = require('express-force-ssl');
 const SSE = require('sse-nodejs');
 
 //  configuration from a designated file.
-nconf.file({file:"settings.json"});
+nconf.file({file: "settings.json"});
 
 //load certificates
 const key = fs.readFileSync('./server/certs/dev.lpchatforconnectorapi.com.key');
-const cert = fs.readFileSync( './server/certs/dev.lpchatforconnectorapi.com.crt');
-const ca = fs.readFileSync( './server/certs/dev.lpchatforconnectorapi.com.crt');
+const cert = fs.readFileSync('./server/certs/dev.lpchatforconnectorapi.com.crt');
+const ca = fs.readFileSync('./server/certs/dev.lpchatforconnectorapi.com.crt');
 
 const options = {
   key: key,
@@ -29,6 +30,7 @@ app.use(cors());
 
 https.createServer(options, app).listen(443);
 app.domains = domains;
+app.domains.init("le92127075");
 
 //Force https
 //app.use(forceSsl);
@@ -37,15 +39,17 @@ app.use("/installation", installationBrige);
 app.use("/account", accountConfBrige);
 app.use("/umsbrige", umsBrigeServer);
 app.use("/notifications", notifications); //receive webhooks notifications
+app.use("/authentication", authenticationBrige); //receive webhooks notifications
 
 //Serve our UI
 app.use(express.static('dist'));
 
 //http server
-app.listen("8282", function() {
+app.listen("8282", function () {
   console.log("listening");
   app.isReady = true;
   app.emit("ready", true);
 });
+
 
 module.exports = app;
