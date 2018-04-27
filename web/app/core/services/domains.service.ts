8db1404f747dadeaa305a0392f6ed5394dc0776e
eplaza <eplaza@liveperson.com>
2018-04-27 16:17:11 +0200

    Domain Header interceptor

--- web/app/core/services/domains.service.ts
+++ web/app/core/services/domains.service.ts
@@ -4,10 +4,12 @@ import {MatSnackBar} from "@angular/material";
 import {HttpClient} from "@angular/common/http";
 import {LoadingService} from "./loading.service";
 import {environment} from '../../../environments/environment';
+import {Subject} from "rxjs/Subject";
 
 @Injectable()
 export class DomainsService extends HttpService{
-
+  public domainsSubject = new Subject();
+  public domains = [];
   constructor(protected snackBar: MatSnackBar,protected http: HttpClient, protected loadingService:LoadingService) {
     super(snackBar,http, loadingService);
   }
@@ -15,10 +17,22 @@ export class DomainsService extends HttpService{
   public getDomainList(brandId: string) {
     this.doGet(`http://${environment.server}/domains/csds/${brandId}`, {}).subscribe((data) => {
       console.log(data);
+      //this.domains = data;
+      let length = data[0].baseURIs.length;
+      for(let i=0; i < length; i++) {
+        this.domains[data[0].baseURIs[i].service] = data[0].baseURIs[i].baseURI;
+      }
+      this.domainsSubject.next('READY');
       this.loadingService.stopLoading();
     }, error => {
       console.log(error);
-      //this.errorResponse(error);
+      this.errorResponse(error);
     });
   }
+
+  public getDomainByServiceName(serviceName: string) {
+    return this.domains[serviceName];
+  }
+
+
 }
