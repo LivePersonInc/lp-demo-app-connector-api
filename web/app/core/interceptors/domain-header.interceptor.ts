373f30209aded523343b2e69013338fd2a72c496
eplaza <eplaza@liveperson.com>
2018-04-27 16:25:49 +0200

    Domains service fixed

--- web/app/core/interceptors/domain-header.interceptor.ts
+++ web/app/core/interceptors/domain-header.interceptor.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import {DomainsService} from "../services/domains.service";
 
 @Injectable()
 export class DomainHeaderInterceptor implements HttpInterceptor {
 
+  constructor(private domainsService: DomainsService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const changedReq = req.clone({headers: req.headers.set('DOMAIN', 'MyHeaderValue')});
+    console.log(req.url);
     return next.handle(changedReq);
   }
 }
