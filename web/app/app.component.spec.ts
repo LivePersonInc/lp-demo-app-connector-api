5d9e223e7c2410f0bc58d8d797561d0c26b4195a
eplaza <eplaza@liveperson.com>
2018-04-30 16:09:48 +0200

    App test fixed

--- web/app/app.component.spec.ts
+++ web/app/app.component.spec.ts
@@ -1,26 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import {RouterModule, RouterOutlet} from "@angular/router";
-import {LpHeaderComponent} from "./lp-header/lp-header.component";
-import {MatSpinner} from "@angular/material";
 import {LpNavComponent} from "./lp-header/lp-nav/lp-nav.component";
 import {LpHeaderModule} from "./lp-header/lp-header.module";
 import {MaterialModule} from "./material.module";
 import {LoadingService} from "./core/services/loading.service";
+import {LpHomeComponent} from "./lp-home/lp-home.component";
+import {LpDemoComponent} from "./lp-demo/lp-demo.component";
+import {LpStartTutorialModule} from "./lp-start-tutorial/lp-start-tutorial.module";
+import {LpTestServicesComponent} from "./lp-test-services/lp-test-services.component";
+import {LpConfirmationDialogComponent} from "./lp-confirmation-dialog/lp-confirmation-dialog.component";
+import {LpConversationModule} from "./lp-conversation/lp-conversation.module";
+import {Router} from "@angular/router";
+import {APP_BASE_HREF} from "@angular/common";
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        LpHomeComponent,
+        LpDemoComponent,
       ],
       schemas: [NO_ERRORS_SCHEMA],
       imports: [
         LpHeaderModule,
-        MaterialModule
+        MaterialModule,
+        LpStartTutorialModule,
+        LpConversationModule,
       ],
-      providers: [LoadingService]
+      providers: [
+        LoadingService,
+        {provide: APP_BASE_HREF, useValue : '/' }
+      ]
     }).compileComponents();
   }));
 
