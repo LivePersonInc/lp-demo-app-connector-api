c1cacb3e38f0e830cf7bc7ee153853f358fbd577
eplaza <eplaza@liveperson.com>
2018-04-30 15:44:18 +0200

    Conversation closed used send row payload

---
+++ server/models/UpdateConversationField.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class UpdateConversationField {
+    constructor(conversationId, conversationField) {
+        this.conversationId = conversationId;
+        this.conversationField = conversationField;
+    }
+}
+
+module.exports = UpdateConversationField;
