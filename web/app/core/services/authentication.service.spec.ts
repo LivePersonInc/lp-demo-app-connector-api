6e042e286700484a33809c4c0ca89bb8d22ef94e
eplaza <eplaza@liveperson.com>
2018-04-30 16:28:03 +0200

    Authentication server fixed

--- web/app/core/services/authentication.service.spec.ts
+++ web/app/core/services/authentication.service.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import {MatSnackBar} from "@angular/material";
+import {HttpClient} from "@angular/common/http";
+import {LoadingService} from "./loading.service";
+import {DomainsService} from "./domains.service";
 
 describe('AuthenticationService', () => {
   beforeEach(() => {
+    const snackBar = jasmine.createSpy('MatSnackBar');
+    const http = jasmine.createSpy('HttpClient');
+    const loadingService = jasmine.createSpy('LoadingService');
+    const domainsService = jasmine.createSpy('DomainService');
     TestBed.configureTestingModule({
-      providers: [AuthenticationService]
+      providers: [
+        AuthenticationService,
+        {provide: DomainsService, useValue: domainsService},
+        {provide: MatSnackBar, useValue: snackBar},
+        {provide: HttpClient, useValue: http},
+        {provide: LoadingService, useValue: loadingService}]
     });
   });
 
