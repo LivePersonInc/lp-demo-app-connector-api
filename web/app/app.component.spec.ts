23e8343fd123eb9a6aee880b5ee2cb80b781fca3
eplaza <eplaza@liveperson.com>
2018-04-30 11:00:49 +0200

    Test fixed and method to extract the key fixed

--- web/app/app.component.spec.ts
+++ web/app/app.component.spec.ts
@@ -1,33 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import {RouterOutlet} from "@angular/router";
+import {RouterModule, RouterOutlet} from "@angular/router";
 import {LpHeaderComponent} from "./lp-header/lp-header.component";
 import {MatSpinner} from "@angular/material";
+import {LpNavComponent} from "./lp-header/lp-nav/lp-nav.component";
+import {LpHeaderModule} from "./lp-header/lp-header.module";
+import {MaterialModule} from "./material.module";
+import {LoadingService} from "./core/services/loading.service";
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        RouterOutlet,
-        LpHeaderComponent,
-        MatSpinner
+        AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        LpHeaderModule,
+        MaterialModule
+      ],
+      providers: [LoadingService]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  /*it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));*/
+
 });
--- web/app/core/interceptors/domain-header.interceptor.spec.ts
+++ web/app/core/interceptors/domain-header.interceptor.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { DomainHeaderInterceptor } from './domain-header.interceptor';
+import {DomainsService} from "../services/domains.service";
+import {MatSnackBar} from "@angular/material";
+import {Router} from "@angular/router";
 
 describe('DomainHeaderInterceptor', () => {
   beforeEach(() => {
+    const domainsService = jasmine.createSpy('DomainsService');
     TestBed.configureTestingModule({
-      providers: [DomainHeaderInterceptor]
+      providers: [DomainHeaderInterceptor, {provide: DomainsService, useValue: domainsService}]
     });
   });
 
-  it('should ...', inject([DomainHeaderInterceptor], (intercptor: DomainHeaderInterceptor) => {
+  it('should inject the service', inject([DomainHeaderInterceptor], (intercptor: DomainHeaderInterceptor) => {
     expect(intercptor).toBeTruthy();
   }));
+
+  it('Should return the first first url element after the domain', inject([DomainHeaderInterceptor], (intercptor: DomainHeaderInterceptor) => {
+    const url = 'http://localhost:8282/account/properties/le92127075';
+    expect(intercptor.getServiceNameByUrl(url)).toBe('account');
+  }));
+
+
 });
