4bb0a6ccf22b44411467c80acabbf8851dd54417
eplaza <eplaza@liveperson.com>
2018-04-30 16:20:14 +0200

    Account config test fixed

--- web/app/core/services/account-config.service.spec.ts
+++ web/app/core/services/account-config.service.spec.ts
@@ -1,11 +1,31 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { AccountConfigService } from './account-config.service';
+import {AuthenticationService} from "./authentication.service";
+import {LoadingService} from "./loading.service";
+import {MatSnackBar} from "@angular/material";
+import {HttpClient} from "@angular/common/http";
+import {Subject} from "rxjs/Subject";
 
 describe('AccountConfigService', () => {
+
   beforeEach(() => {
+    const authenticationService = { //MOCK
+      userLoggedSubject:{
+        subscribe: () => {}
+      }
+    };
+    const snackBar = jasmine.createSpy('MatSnackBar');
+    const http = jasmine.createSpy('HttpClient');
+    const loadingService = jasmine.createSpy('LoadingService');
+
     TestBed.configureTestingModule({
-      providers: [AccountConfigService]
+      providers: [
+        AccountConfigService,
+        {provide: AuthenticationService, useValue: authenticationService},
+        {provide: MatSnackBar, useValue: snackBar},
+        {provide: HttpClient, useValue: http},
+        {provide: LoadingService, useValue: loadingService}]
+
     });
   });
 
