db875476344ec1b0bfeae5c6ddf620af03fa5bf0
eplaza <eplaza@liveperson.com>
2018-04-30 14:30:48 +0200

    start tutorial Module Fixed

---
+++ web/app/lp-conversation/lp-conversation.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LpConversationComponent} from "./lp-conversation.component";
+import {LpConsoleComponent} from "./lp-console/lp-console.component";
+import {LpChatBoxComponent} from "./lp-chat-box/lp-chat-box.component";
+import {LpChatBoxFooterComponent} from "./lp-chat-box/lp-chat-box-footer/lp-chat-box-footer.component";
+import {LpChatBoxMessageComponent} from "./lp-chat-box/lp-chat-box-message/lp-chat-box-message.component";
+import {MaterialModule} from "../material.module";
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+  ],
+  declarations: [
+    LpConversationComponent,
+    LpConsoleComponent,
+    LpChatBoxComponent,
+    LpChatBoxFooterComponent,
+    LpChatBoxMessageComponent
+  ],
+  exports: [
+    LpConversationComponent,
+    LpConsoleComponent,
+    LpChatBoxComponent,
+    LpChatBoxFooterComponent,
+    LpChatBoxMessageComponent
+  ]
+})
+export class LpConversationModule { }
