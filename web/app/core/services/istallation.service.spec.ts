9cd436e93e510b0bb1eb978b01e04ff6171f93b9
eplaza <eplaza@liveperson.com>
2018-05-02 08:47:10 +0200

    Services tests fixed

--- web/app/core/services/istallation.service.spec.ts
+++ web/app/core/services/istallation.service.spec.ts
@@ -1,11 +1,29 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { InstallationService } from './istallation.service';
+import {AuthenticationService} from "./authentication.service";
+import {MatSnackBar} from "@angular/material";
+import {HttpClient} from "@angular/common/http";
+import {LoadingService} from "./loading.service";
 
 describe('InstallationService', () => {
   beforeEach(() => {
+    const authenticationService = { //MOCK
+      userLoggedSubject:{
+        subscribe: () => {}
+      }
+    };
+    const snackBar = jasmine.createSpy('MatSnackBar');
+    const http = jasmine.createSpy('HttpClient');
+    const loadingService = jasmine.createSpy('LoadingService');
     TestBed.configureTestingModule({
-      providers: [InstallationService]
+      providers: [
+        InstallationService,
+        {provide: AuthenticationService, useValue: authenticationService},
+        {provide: MatSnackBar, useValue: snackBar},
+        {provide: HttpClient, useValue: http},
+        {provide: LoadingService, useValue: loadingService}
+      ]
     });
   });
 
