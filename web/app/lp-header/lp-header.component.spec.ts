128e22f310e448f2bd770fdbbc2aef35c046e901
eplaza <eplaza@liveperson.com>
2018-05-02 15:36:03 +0200

    test fixed

--- web/app/lp-header/lp-header.component.spec.ts
+++ web/app/lp-header/lp-header.component.spec.ts
@@ -4,15 +4,19 @@ import { LpHeaderComponent } from './lp-header.component';
 import {LpNavComponent} from "./lp-nav/lp-nav.component";
 import {LpNavMenuComponent} from "./lp-nav-menu/lp-nav-menu.component";
 import {MaterialModule} from "../material.module";
+import {AuthenticationService} from "../core/services/authentication.service";
 
 describe('LpHeaderComponent', () => {
   let component: LpHeaderComponent;
   let fixture: ComponentFixture<LpHeaderComponent>;
 
   beforeEach(async(() => {
+    const authenticationService = jasmine.createSpy('AuthenticationService');
+
     TestBed.configureTestingModule({
       imports: [MaterialModule],
-      declarations: [ LpHeaderComponent, LpNavComponent, LpNavMenuComponent ]
+      declarations: [ LpHeaderComponent, LpNavComponent, LpNavMenuComponent ],
+      providers: [{provide: AuthenticationService, useValue: authenticationService}]
     })
     .compileComponents();
   }));
--- web/app/lp-header/lp-nav/lp-nav.component.spec.ts
+++ web/app/lp-header/lp-nav/lp-nav.component.spec.ts
@@ -2,15 +2,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LpNavComponent } from './lp-nav.component';
 import {MaterialModule} from "../../material.module";
+import {AuthenticationService} from "../../core/services/authentication.service";
 
 describe('LpNavComponent', () => {
   let component: LpNavComponent;
   let fixture: ComponentFixture<LpNavComponent>;
 
   beforeEach(async(() => {
+    const authenticationService = jasmine.createSpy('AuthenticationService');
     TestBed.configureTestingModule({
       imports: [MaterialModule],
-      declarations: [ LpNavComponent ]
+      declarations: [ LpNavComponent ],
+      providers: [
+        {provide:AuthenticationService, useValue: authenticationService}
+      ]
     })
     .compileComponents();
   }));
