e943228e62f021ac8221eb7dbf4d94979d699530
eplaza <eplaza@liveperson.com>
2018-05-02 08:47:10 +0200

    Services tests fixed

--- web/app/core/services/send-api.service.spec.ts
+++ web/app/core/services/send-api.service.spec.ts
@@ -1,16 +1,29 @@
-/*import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import {SendApiService} from './send-api.service';
+import {MatSnackBar} from "@angular/material";
+import {HttpClient} from "@angular/common/http";
+import {LoadingService} from "./loading.service";
+import {DomainsService} from "./domains.service";
 
-import { AppInstallationApiService } from './app-installation-api.service';
-
-describe('AppInstallationApiService', () => {
+describe('SendApiService', () => {
   beforeEach(() => {
+    const snackBar = jasmine.createSpy('MatSnackBar');
+    const http = jasmine.createSpy('HttpClient');
+    const loadingService = jasmine.createSpy('LoadingService');
+    const domainsService = jasmine.createSpy('DomainService');
     TestBed.configureTestingModule({
-      providers: [AppInstallationApiService]
+      providers: [
+        SendApiService,
+        {provide: DomainsService, useValue: domainsService},
+        {provide: MatSnackBar, useValue: snackBar},
+        {provide: HttpClient, useValue: http},
+        {provide: LoadingService, useValue: loadingService}
+      ]
     });
   });
 
-  it('should be created', inject([AppInstallationApiService], (service: AppInstallationApiService) => {
+  it('should be created', inject([SendApiService], (service: SendApiService) => {
     expect(service).toBeTruthy();
   }));
 });
-*/
+
