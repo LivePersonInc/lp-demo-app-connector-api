dc15a9db05d49d77335e01c117e94247a3d5de59
eplaza <eplaza@liveperson.com>
2018-05-02 15:27:05 +0200

    Disabled demo page when user is not loogedin

--- web/app/lp-header/lp-nav/lp-nav.component.ts
+++ web/app/lp-header/lp-nav/lp-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../core/services/authentication.service";
 
 @Component({
   selector: 'lp-nav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LpNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
