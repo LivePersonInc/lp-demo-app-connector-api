1a6b806ac2606492a17f65d347c80bf4595f0022
eplaza <eplaza@liveperson.com>
2018-05-02 11:31:18 +0200

    Test fixed and code refactored

--- web/app/lp-conversation/lp-conversation.component.spec.ts
+++ web/app/lp-conversation/lp-conversation.component.spec.ts
@@ -1,14 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LpConversationComponent } from './lp-conversation.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Conversation} from "../shared/models/conversation/conversation";
+import {LpConversationComponent} from './lp-conversation.component';
+import {MaterialModule} from "../material.module";
+import {FormsModule} from "@angular/forms";
+import {LpChatBoxComponent} from "./lp-chat-box/lp-chat-box.component";
+import {LpConsoleComponent} from "./lp-console/lp-console.component";
+import {LpConversationModule} from "./lp-conversation.module";
+import {LpChatBoxMessageComponent} from "./lp-chat-box/lp-chat-box-message/lp-chat-box-message.component";
+import {LpChatBoxFooterComponent} from "./lp-chat-box/lp-chat-box-footer/lp-chat-box-footer.component";
+import {AuthenticationService} from "../core/services/authentication.service";
+import {ConversationService} from "../core/services/conversation.service";
+import {InstallationService} from "../core/services/istallation.service";
 
 describe('LpConversationComponent', () => {
   let component: LpConversationComponent;
   let fixture: ComponentFixture<LpConversationComponent>;
 
   beforeEach(async(() => {
+    const authenticationService = { //MOCK
+      userLoggedSubject:{
+        subscribe: () => {}
+      }
+    };
+    const conversationService = jasmine.createSpy( 'ConversationService');
+    const installationService = jasmine.createSpy( 'InstallationService');
+
     TestBed.configureTestingModule({
-      declarations: [ LpConversationComponent ]
+      imports: [MaterialModule, FormsModule],
+      declarations: [
+        LpConversationComponent,
+        LpChatBoxComponent,
+        LpConsoleComponent,
+        LpChatBoxMessageComponent,
+        LpChatBoxFooterComponent
+      ],
+      providers: [
+        {provide: AuthenticationService, useValue: authenticationService},
+        {provide: ConversationService, useValue: conversationService},
+        {provide: InstallationService, useValue: installationService},
+      ]
     })
     .compileComponents();
   }));
