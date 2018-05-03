eafee9c1101275527461f9c9c66a3b363a400901
eplaza <eplaza@liveperson.com>
2018-05-03 09:50:34 +0200

    Enviromment files refactored

--- web/environments/environment.ts
+++ web/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: false,
-  webhookServer: "https://17ddc37d.ngrok.io/notifications/event",
   server: "localhost:8282",
-  idp:"qtvr-wng113.dev.lprnd.net",
-  brandId: "le92127075",
-  username:"epbot@liveperson.com",
-  appKey: "7791627d-ce48-4363-8943-ac9c8fa7ca3b",
-  appSecret: "c79p3jifp73ohupjkvirjkfj9e",
   //Services names to get the domain with the CSDS
   authentication: "agentVep",
   installation:"accountConfigReadWrite",
