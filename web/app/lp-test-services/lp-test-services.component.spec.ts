e19cdbf0ffbd7b150705ce5546c4cc8e64c9e0b5
eplaza <eplaza@liveperson.com>
2018-05-03 08:29:17 +0200

    test fixed and code refactored

--- web/app/lp-test-services/lp-test-services.component.spec.ts
+++ web/app/lp-test-services/lp-test-services.component.spec.ts
@@ -1,12 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LpTestServicesComponent } from './lp-test-services.component';
 
 describe('LpTestServicesComponent', () => {
   let component: LpTestServicesComponent;
 
   beforeEach(() => {
-    component = new LpTestServicesComponent(null,null,null);
+    component = new LpTestServicesComponent(null,null,null,null,null);
   });
 
   it('should create', () => {
