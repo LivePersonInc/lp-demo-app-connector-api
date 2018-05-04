69365c4b23e46a7f53c52fd1c06d8e9efca2864b
eplaza <eplaza@liveperson.com>
2018-05-04 14:56:17 +0200

    error fixed

--- web/app/core/interceptors/domain-header.interceptor.ts
+++ web/app/core/interceptors/domain-header.interceptor.ts
@@ -13,9 +13,7 @@ export class DomainHeaderInterceptor implements HttpInterceptor {
     const domainKey = this.getServiceNameByUrl(req.url);
     let changedReq = req.clone();
     if(environment[domainKey]){
-      console.log(environment[domainKey]);
       const domainHeader = this.domainsService.getDomainByServiceName(environment[domainKey]);
-      console.log(domainHeader);
       changedReq = req.clone({headers: req.headers.set('LP-DOMAIN', domainHeader)});
     }
     return next.handle(changedReq);
