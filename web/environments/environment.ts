813ed8420f6d4d70d5f82c5d0f541e3494d2b676
eplaza <eplaza@liveperson.com>
2018-08-10 11:42:00 +0200

    LE-89698 Server port unused prop removed

--- web/environments/environment.ts
+++ web/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   server: "localhost",
-  server_port: 8282,
   //Services names to get the domain with the CSDS
   authentication: "agentVep",
   installation:"accountConfigReadWrite",
