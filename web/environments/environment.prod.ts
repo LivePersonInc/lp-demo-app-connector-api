3bb7d0cf88821255b03aadc008e05727d5c46862
eplaza <eplaza@liveperson.com>
2018-08-15 13:52:38 +0200

    LE-89698 updated git history

--- web/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  server: "localhost",
-  //Services names to get the domain with the CSDS
-  authentication: "agentVep",
-  installation:"accountConfigReadWrite",
-  ums:"asyncMessagingEnt",
-  account: "accountConfigReadWrite",
-};
