ffd5bc4f2bdac31027e2722ea87473b6432e0d79
eplaza <eplaza@liveperson.com>
2018-08-15 10:29:25 +0200

    LE-89698 Subcribed to notifications URL fixed

---
+++ web/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  server: "localhost",
+  //Services names to get the domain with the CSDS
+  authentication: "agentVep",
+  installation:"accountConfigReadWrite",
+  ums:"asyncMessagingEnt",
+  account: "accountConfigReadWrite",
+};
