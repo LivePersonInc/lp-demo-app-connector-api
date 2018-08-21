69ce465732e2244e32a291e65ab341b755904950
eplaza <eplaza@liveperson.com>
2018-08-21 15:09:29 +0200

    unused injections removed

--- web/app/lp-home/lp-home.component.ts
+++ web/app/lp-home/lp-home.component.ts
@@ -7,8 +7,6 @@ import {Router} from "@angular/router";
 import {InstallationService} from "../core/services/istallation.service";
 import {LpConfirmationDialogComponent} from "../lp-confirmation-dialog/lp-confirmation-dialog.component";
 import {MatDialog} from "@angular/material";
-import {ConversationService} from "../core/services/conversation.service";
-import {AccountConfigService} from "../core/services/account-config.service";
 import {DomainsService} from "../core/services/domains.service";
 
 @Component({
@@ -32,8 +30,6 @@ export class LpHomeComponent implements OnInit, OnDestroy {
 
   constructor(private _authenticationService: AuthenticationService,
               private installationService: InstallationService,
-              private conversationService: ConversationService,
-              private accountConfigService: AccountConfigService,
               private domainsService: DomainsService,
               private router: Router,
               private fb: FormBuilder,
