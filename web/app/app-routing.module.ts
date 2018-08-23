697ad7fec8d1fc5a810218d720c39942f2daf04c
eplaza <eplaza@liveperson.com>
2018-08-23 11:41:19 +0200

    Unused module removed

--- web/app/app-routing.module.ts
+++ web/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LpHomeComponent} from "./lp-home/lp-home.component";
-import {LpConversationComponent} from "./lp-conversation/lp-conversation.component";
-import {LpTestServicesComponent} from "./lp-test-services/lp-test-services.component";
 import {LpDemoComponent} from "./lp-demo/lp-demo.component";
 import {LpStartTutotialComponent} from "./lp-start-tutorial/lp-start-tutotial.component";
 import {AuthGuardGuard} from "./core/guards/auth-guard.guard";
