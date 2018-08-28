267aa2c1dae3f813d200ffb82db0a1e8d58772d5
eplaza <eplaza@liveperson.com>
2018-08-28 09:41:34 +0200

    Check if message was yesterday or today

--- web/app/components/lp-conversation/lp-chat-box/lp-chat-box-message/lp-chat-box-message.component.ts
+++ web/app/components/lp-conversation/lp-chat-box/lp-chat-box-message/lp-chat-box-message.component.ts
@@ -17,5 +17,23 @@ export class LpChatBoxMessageComponent implements OnInit {
   ngOnInit() {
   }
 
+  public isTodayMessage(): boolean {
+    let messageDate = new Date(this.message.timestamp);
+    let today = new Date();
+
+    return (messageDate.getFullYear() === today.getFullYear())  &&
+      (messageDate.getMonth() === today.getMonth()) &&
+      (messageDate.getDay() === today.getDay());
+  }
+
+  public isYesterdayMessage(): boolean {
+    let messageDate = new Date(this.message.timestamp);
+    let today = new Date();
+
+    return (messageDate.getFullYear() === today.getFullYear())  &&
+      (messageDate.getMonth() === today.getMonth()) &&
+      (messageDate.getDay() === today.getDay()-1);
+  }
+
 
 }
