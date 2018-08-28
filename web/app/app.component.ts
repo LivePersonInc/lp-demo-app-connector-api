7a697a4b2fbdc02803b6eb7123065a0ae6802b7f
eplaza <eplaza@liveperson.com>
2018-08-28 08:56:27 +0200

    " changed for  '

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Subscription} from "rxjs/Subscription";
-import {LoadingService} from "./core/services/loading.service";
+import {Subscription} from 'rxjs/Subscription';
+import {LoadingService} from './core/services/loading.service';
 
 @Component({
   selector: 'app-root',
