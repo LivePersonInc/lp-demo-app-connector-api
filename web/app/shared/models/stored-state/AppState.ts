d35ae840dea09dd5eb17ed39a88dc9dcaba69f2d
eplaza <eplaza@liveperson.com>
2018-09-11 10:18:33 +0200

    AppState object simplified with the minimum necessary data.
    This will improve the security issues with sensitive data
    but not solve it completely

--- web/app/shared/models/stored-state/AppState.ts
+++ web/app/shared/models/stored-state/AppState.ts
@@ -1,16 +1,11 @@
-import {Conversation} from "../conversation/conversation.model";
-import {AppInstall} from "../app-installation/appInstall.model";
 import {Deserializable} from "../deserializable.model";
 
 export class AppState implements Deserializable<AppState>{
-  lastConversation: Conversation;
-  isAsyncMessagingActive: boolean;
-  selectedApp: AppInstall;
+  conversationId: string;
+  appId: string;
 
   deserialize(input: any): AppState {
     Object.assign(this, input);
-    input.lastConversation ? this.lastConversation = new Conversation(null, null, null, null).deserialize(input.lastConversation): this.lastConversation = null;
-    input.selectedApp ? this.selectedApp = new AppInstall( ).deserialize(input.selectedApp): this.selectedApp = null;
     return this;
   }
 }
