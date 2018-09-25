d04e9e3df30609258f93778fedd0b34fa52f035c
eplaza <eplaza@liveperson.com>
2018-09-25 14:01:58 +0200

    Bug fixed with csds domains

--- web/environments/environment.prod.ts
+++ web/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   server: "localhost",
+  port: "8282",
   //Services names to get the domain with the CSDS
   authentication: "agentVep",
   installation:"accountConfigReadWrite",
--- web/environments/environment.ts
+++ web/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   server: "localhost",
+  port: "8282",
   //Services names to get the domain with the CSDS
   authentication: "agentVep",
   installation:"accountConfigReadWrite",
