c3238c52fccc6b941178fe16fb6b0e97201d014c
eplaza <eplaza@liveperson.com>
2018-10-18 10:53:26 +0200

    protocol added to web environment properties

--- web/environments/environment.prod.ts
+++ web/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   server: location.hostname,
+  protocol: "http",
   port: "8282",
   //Services names to get the domain with the CSDS
   authentication: "agentVep",
--- web/environments/environment.ts
+++ web/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   server: location.hostname,
+  protocol: "http",
   port: "8282",
   //Services names to get the domain with the CSDS
   authentication: "agentVep",
