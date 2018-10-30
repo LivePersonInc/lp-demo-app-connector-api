149777d8249344bdbf1433c2f609ade31c814418
eplaza <eplaza@liveperson.com>
2018-10-30 14:42:40 +0100

    added karma-chrome-launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -33,4 +32,4 @@ module.exports = function (config) {
     browsers: ['ChromeHeadless'],
     singleRun: false
   });
-};
+}
