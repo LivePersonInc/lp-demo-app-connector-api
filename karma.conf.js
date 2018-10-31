8ed33c0b773c24df53c047bc55a70f6276175c88
eplaza <eplaza@liveperson.com>
2018-10-31 09:01:21 +0100

    Karma fix

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision;
-const Downloader = require('puppeteer/utils/ChromiumDownloader');
-const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision);
 
-process.env.CHROME_BIN = revisionInfo.executablePath;
+const puppeteer = require('puppeteer');
+
+process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
   config.set({
