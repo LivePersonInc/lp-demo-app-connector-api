10c1ff9c2c7d578037eeb6dd4ca83a1b62b0222b
eplaza <eplaza@liveperson.com>
2018-10-31 08:46:10 +0100

    Karma config changed: Chromium added

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision;
+const Downloader = require('puppeteer/utils/ChromiumDownloader');
+const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision);
+
+process.env.CHROME_BIN = revisionInfo.executablePath;
 
 module.exports = function (config) {
   config.set({
@@ -32,4 +36,4 @@ module.exports = function (config) {
     browsers: ['ChromeHeadless'],
     singleRun: false
   });
-}
+};
