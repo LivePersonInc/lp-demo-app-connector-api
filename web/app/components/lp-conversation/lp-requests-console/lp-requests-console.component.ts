ab80c354dc2e20b46f0167c93cd6bf8825c9c8a7
eplaza <eplaza@liveperson.com>
2018-11-09 11:45:16 +0100

    New component in demo added

--- web/app/components/lp-conversation/lp-requests-console/lp-requests-console.component.ts
+++ web/app/components/lp-conversation/lp-requests-console/lp-requests-console.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Conversation} from "../../../shared/models/conversation/conversation.model";
 
 @Component({
-  selector: 'app-lp-requests-console',
+  selector: 'lp-requests-console',
   templateUrl: './lp-requests-console.component.html',
   styleUrls: ['./lp-requests-console.component.scss']
 })
 export class LpRequestsConsoleComponent implements OnInit {
+  @Input()
+  public conversation: Conversation;
+
+  @ViewChild('notifications') private notifications: ElementRef;
 
   constructor() { }
 
