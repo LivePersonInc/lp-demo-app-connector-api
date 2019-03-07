929c1303f5119a6c625aa01d2160f481e86cc980
eplaza <eplaza@liveperson.com>
2019-03-07 16:29:39 +0100

    removed unnecessary polyfills

--- web/polyfills.ts
+++ web/polyfills.ts
@@ -82,10 +82,10 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-import 'intl';  // Run `npm install --save intl`.
+//mport 'intl';  // Run `npm install --save intl`.
 /**
  * Need to import at least one locale-data with intl.
  */
-import 'intl/locale-data/jsonp/en';
+// simport 'intl/locale-data/jsonp/en';
 
 import 'url-polyfill'; //Needed for phantonJS
