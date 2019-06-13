f63ef235ec2f8872691ebb006cd3f4b3e465174d
slacki123 <sebastian.lacki1@gmail.com>
2019-06-13 15:56:47 +0100

    structured content is now parsed. QR dont work

--- web/app/components/lp-conversation/lp-chat-box/lp-chat-box-message/lp-chat-box-message.component.ts
+++ web/app/components/lp-conversation/lp-chat-box/lp-chat-box-message/lp-chat-box-message.component.ts
@@ -1,22 +1,46 @@
-import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
-import {ChatMessage, MessageType} from "../../../../shared/models/conversation/chatMessage.model";
-import {FileMessage} from "../../../../shared/models/conversation/fileMessage.model";
+import {Component, Input, Output, OnInit, AfterViewInit, EventEmitter, Renderer, Renderer2, ElementRef, ViewChild} from '@angular/core';
+import {ChatMessage, MessageType} from '../../../../shared/models/conversation/chatMessage.model';
+import {FileMessage} from '../../../../shared/models/conversation/fileMessage.model';
+import * as JsonPollock from '../../../../../../node_modules/json-pollock/dist/json-pollock.bundle.min.js';
+
+
 
 @Component({
   selector: 'lp-chat-box-message',
   templateUrl: './lp-chat-box-message.component.html',
   styleUrls: ['./lp-chat-box-message.component.scss']
 })
-export class LpChatBoxMessageComponent implements OnInit {
+export class LpChatBoxMessageComponent implements OnInit, AfterViewInit {
   @Input() public message: ChatMessage;
   @Output() download = new EventEmitter<FileMessage>();
 
   public messageType: string;
 
-  constructor() {}
+
+  @ViewChild('structuredContent') d1: ElementRef;
+
+  constructor (private renderer: Renderer2){
+
+  }
+
+  public addElement(content) {
+    const rooEl = JsonPollock.render(content);
+    console.log(rooEl);
+
+    const structuredContentElement: HTMLParagraphElement = rooEl;
+    this.renderer.appendChild(this.d1.nativeElement, structuredContentElement);
+  }
+
+  ngAfterViewInit(): void {
+    //TODO: Seb
+    if(this.message.message.content) {
+      //Mind that this doesn't work for quick replies.
+      this.addElement(this.message.message.content)
+    }
+  }
 
   ngOnInit() {
-    this.messageType = MessageType[this.message.type].toLocaleLowerCase()
+    this.messageType = MessageType[this.message.type].toLocaleLowerCase();
   }
 
   public isTodayMessage(): boolean {
