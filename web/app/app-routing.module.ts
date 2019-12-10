dad41bac900f05ad172c1d0dfbd698b9c631f481
eplaza <eplaza@liveperson.com>
2019-12-10 10:40:21 +0100

    Routing fixed

--- web/app/app-routing.module.ts
+++ web/app/app-routing.module.ts
@@ -5,13 +5,11 @@ import {AuthGuardGuard} from "./core/guards/auth-guard.guard";
 import {LogoutGuard} from "./core/guards/logout.guard";
 import {LpLoginComponent} from "./components/lp-login/lp-login.component";
 import {LpHomeComponent} from "./components/lp-home/lp-home.component";
-import {LpAppInstallationsComponent} from "./components/lp-app-installations/lp-app-installations.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: LpHomeComponent, canActivate: [AuthGuardGuard]},
   {path: 'login', component: LpLoginComponent},
-  {path: 'appinstalls', component: LpAppInstallationsComponent, canActivate: [AuthGuardGuard]},
   {path: 'logout', component: LpLoginComponent, canActivate: [LogoutGuard]},
   {path: 'demo', component: LpDemoComponent, canActivate: [AuthGuardGuard]},
 ];
