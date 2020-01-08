060accc12f8a889fe8440c86a919c98901bb9f03
eplaza <eplaza@liveperson.com>
2020-01-08 11:08:16 +0100

    OnDestroy in app component

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {LoadingService} from './core/services/loading.service';
 
@@ -7,21 +7,27 @@ import {LoadingService} from './core/services/loading.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
   public isLoading = false;
   private subscription: Subscription;
-
+  
   constructor(private loadingService: LoadingService) {
-
+  
   }
-
+  
   ngOnInit() {
-    this.subscription = this.loadingService.isLoadingSubscription().subscribe( isLoading => {
+    this.subscription = this.loadingService.isLoadingSubscription().subscribe(isLoading => {
       this.isLoading = isLoading;
     }, error => {
       console.log('SUBSCRIPTION ERROR: ' + error);
     });
   }
-
-
+  
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+  
+  
 }
--- web/app/core/services/loading.service.ts
+++ web/app/core/services/loading.service.ts
@@ -1,36 +1,35 @@
-import { Injectable } from '@angular/core';
-import {Subject} from 'rxjs';
-import {Observable} from "rxjs";
+import {Injectable} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 
 @Injectable()
 export class LoadingService {
-  private isServiceActive:boolean;
+  private isServiceActive: boolean;
   private loadingSubject = new Subject<boolean>();
-
+  
   constructor() {
     this.activateLoadingService();
   }
-
+  
   public isLoadingSubscription(): Observable<boolean> {
     return this.loadingSubject.asObservable();
   }
-
+  
   public stopLoading() {
     this.loadingSubject.next(false);
   }
-
+  
   public startLoading() {
-    if(this.isServiceActive){
+    if (this.isServiceActive) {
       this.loadingSubject.next(true);
     }
   }
-
+  
   public activateLoadingService() {
     this.isServiceActive = true;
   }
-
+  
   public deactivateLoadingService() {
     this.isServiceActive = false;
   }
-
+  
 }
